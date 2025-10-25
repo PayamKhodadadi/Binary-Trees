261a808b9e3572da6e6aae38b79d2762a946b7a3
payam khodadadi <payamkhodadadi92@gmail.com>
2025-10-25 15:15:12 +0330

    LeetCode Challenge

--- binary-search-tree.js
+++ binary-search-tree.js
@@ -98,4 +98,18 @@ class BinarySearchTree {
   }
 }
 
+const isSameTree = function (p, q) {
+  if (p === null && q === null) return true;
+  if (p === null || q === null) return false;
+
+  if (p.val !== q.val) return false;
+
+  return isSameTree(p.left, q.left) && isSameTree(p.right, q.right);
+};
+let p = new TreeNode(1);
+p.left = new TreeNode(2);
+let q = new TreeNode(1);
+q.right = new TreeNode(2);
+console.log(isSameTree(p, q)); //false
+
 module.exports = { BinarySearchTree, TreeNode };
